b434bcecbce8cee2e49fd1e58845ef0ece03da60
ElianArism <elianarism@gmail.com>
2020-10-27 20:14:28 -0300

    ,

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express(); 
 
-app.use(express.static(__dirname + 'dist/fzindumentaria'));
+app.use(express.static(__dirname + './dist/fzindumentaria'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'dist/fzindumentaria/index.html')); 
+    res.sendFile(path.join(__dirname + './dist/fzindumentaria/index.html')); 
 }); 
 
 app.listen(process.env.PORT || 8080);
