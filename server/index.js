a4e4bf8c9d237846b716bdf09d5b3ae05204a1c8
ElianArism <elianarism@gmail.com>
2020-10-27 20:40:47 -0300

    heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express(); 
 
-app.use(express.static(__dirname + './dist/fzindumentaria'));
+app.use(express.static('./dist/fzindumentaria'));
 
 app.get('/', (req, res) => {
     res.sendFile('./dist/fzindumentaria/index.html'); 
